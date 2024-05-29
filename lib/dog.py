774ee59f146f6d25709d0dd9fb9c1cde11bd9694
gatere <mercynjerigatere@gmail.com>
2024-05-29 11:29:28 +0300

    Adds lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "dog", breed = "Mastiff"):
+        if (isinstance(name, str)  and (0 < len(name) < 25)):
+            self.name = name
+        else :
+            print("Name must be string between 1 and 25 characters.")
+        if (breed in APPROVED_BREEDS):
+            self.breed = breed
+        else :
+            print("Breed must be in list of approved breeds.")
+
+fido = Dog("Fido", "Corgi")
+print (fido.name)
+print (fido.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "User", job = "Admin"):
+        if ((isinstance(name, str)) and (0 < len(name) < 25)):
+            self.name = name.title()
+        else :
+            print("Name must be string between 1 and 25 characters.")
+        if (job in APPROVED_JOBS):
+            self.job = job
+        else :
+            print("Job must be in list of approved jobs.")
+
+john = Person("john doe", "Sales")
+print (john.name)
+print (john.job)
